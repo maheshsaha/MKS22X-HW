3ec9f20d1848ec2793cb428f2006ad2807da9ed0
Mahesh Saha <msaha1@stuy.edu>
2017-02-06 19:04:37 -0500

    Finished sqrt function and Recursion file!

--- HW01Recursion/Recursion.java
+++ HW01Recursion/Recursion.java
@@ -1,9 +1,24 @@
 public class Recursion {
+    public static String name(){
+	return "Saha,Mahesh";
+    }
+    
     public static double sqrt (double n){
 	if (n < 0)
 	    throw new IllegalArgumentException();
 	else{
-	    return 0.0;
+	    return rHelper(n, 1);
 	}
+    }
+    public static double rHelper(double n, double guess){
+	double bGuess = (n/guess + guess)/2;
+	if ((bGuess * bGuess)-n < 0.00000000000001)
+	    return bGuess;
+	return rHelper(n, bGuess);
+    }
+
+    public static void main(String[]args){
+	System.out.println(sqrt(10000));
+	System.out.println(name());
     }
 }
