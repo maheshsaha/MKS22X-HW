3cad4dd6771ed09800c48363bfe8222fde41ac83
Mahesh Saha <msaha1@stuy.edu>
2017-02-08 01:07:00 -0500

    Reran test cases, fixed error for 0

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -6,19 +6,21 @@ public class Recursion {
     public static double sqrt (double n){
 	if (n < 0)
 	    throw new IllegalArgumentException();
+	else if (n == 0)
+	    return 0;
 	else{
 	    return rHelper(n, 1);
 	}
     }
     public static double rHelper(double n, double guess){
 	double bGuess = (n/guess + guess)/2;
-	if ((bGuess * bGuess)-n < 0.00000000000001)
+	if ((bGuess * bGuess)-n < 0.0000000001)
 	    return bGuess;
 	return rHelper(n, bGuess);
     }
 
-    public static void main(String[]args){
-	System.out.println(sqrt(10000));
-	System.out.println(name());
+    // public static void main(String[]args){
+    // 	System.out.println(sqrt(1000));
+    // 	System.out.println(name());
     }
 }
