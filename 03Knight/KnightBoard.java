a67482e89dff354d91824232c436d7d95bbd6960
Mahesh Saha <sahamahesh20@gmail.com>
2017-02-16 12:28:31 -0500

    Made a few changes, still doesn't work.

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -20,9 +20,9 @@ public class KnightBoard{
     }
 
     private boolean isGood(int r, int c){
-	return true;
+	return (board[r][c] == 0 && r < board.length && c < board[r].length);
     }
-
+    // heuristic move eval: choose the knight with the least number of moves available to start. Continue tree progression by choosing the knight with the least moves available. Start top left corner and progress
     private boolean solveH(int r, int c, int l){ // l is the number of the knight
 	if(l > board.length*board[0].length)
 	    return true;
@@ -30,9 +30,18 @@ public class KnightBoard{
 	    for (int w = 0; w < board.length; w++){
 		if (board[w][c] == 0) {
 		    addKnight(w,c);
-		    if (!solveH(w,c+1){
+		    if (!solveH(w+2,c+1)){
 			removeKnight(w,c);
 		    }
+		    else if (!solveH(w-2,c+1)){
+			removeKnight(w,c);
+		    }
+		    else if (!solveH(w+1,c+2)){
+			removeKnight(w,c);
+		    }
+		    else if (!solveH(w-1,c+2)){
+			removeKnight(w,c);
+			}
 		    else
 			return true;
 		}
@@ -40,4 +49,6 @@ public class KnightBoard{
 	    return false;
 	}
     }
-    
+}
+
+
