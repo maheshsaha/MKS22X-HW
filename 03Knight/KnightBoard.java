29bdb8d04d9e987b05a27e15af5d2c1670504e22
Mahesh Saha <msaha1@stuy.edu>
2017-02-16 21:53:14 -0500

    Finished toString

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -5,50 +5,40 @@ public class KnightBoard{
     public KnightBoard(int r, int c){
 	board = new int[r][c];
 	for (int i = 0; i < board.length; i ++){
-	    for(int j = 0; j < board[i].length; j ++){
+	    for (int j = 0; j < board[i].length; j ++){
 		board[i][j] = 0;
 	    }
 	}
     }
     
     public String toString(){
-	return "";
+	String b = "";
+	for (int i = 0; i < board.length; i ++){
+	    b += "\n";
+	    for (int j = 0; j < board[0].length; j ++){
+		if (board[i][j] < 10)
+		    b += " " + board[i][j] + " ";
+		else
+		    b += "" + board[i][j] + " ";
+	    }
+	}
+	return b;
     }
 
     public void solve(){
-	solveH(0,0,1);
-    }
-
-    private boolean isGood(int r, int c){
-	return (board[r][c] == 0 && r < board.length && c < board[r].length);
-    }
-    // heuristic move eval: choose the knight with the least number of moves available to start. Continue tree progression by choosing the knight with the least moves available. Start top left corner and progress
-    private boolean solveH(int r, int c, int l){ // l is the number of the knight
-	if(l > board.length*board[0].length)
-	    return true;
-	else {
-	    for (int w = 0; w < board.length; w++){
-		if (board[w][c] == 0) {
-		    addKnight(w,c);
-		    if (!solveH(w+2,c+1)){
-			removeKnight(w,c);
-		    }
-		    else if (!solveH(w-2,c+1)){
-			removeKnight(w,c);
-		    }
-		    else if (!solveH(w+1,c+2)){
-			removeKnight(w,c);
-		    }
-		    else if (!solveH(w-1,c+2)){
-			removeKnight(w,c);
-			}
-		    else
-			return true;
-		}
+	for (int i = 0; i < board.length; i ++){
+	    for (int j = 0; j < board[i].length; j ++){
+		if(solveH(i,j,1))
+		    return;
 	    }
-	    return false;
 	}
     }
+    
+    private boolean solveH(int row, int col, int level){
+	return false;   
+    }
+
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
 }
-
-
