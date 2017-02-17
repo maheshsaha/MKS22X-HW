997ac045a2c8004dc5c835c5d4921856d05e7068
Mahesh Saha <msaha1@stuy.edu>
2017-02-16 22:36:13 -0500

    Finished Knights Tour!

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -4,17 +4,13 @@ public class KnightBoard{
 
     public KnightBoard(int r, int c){
 	board = new int[r][c];
-	for (int i = 0; i < board.length; i ++){
-	    for (int j = 0; j < board[i].length; j ++){
-		board[i][j] = 0;
-	    }
-	}
+	
     }
     
     public String toString(){
 	String b = "";
 	for (int i = 0; i < board.length; i ++){
-	    b += "\n";
+	    if (i != 0)  b += "\n";
 	    for (int j = 0; j < board[0].length; j ++){
 		if (board[i][j] < 10)
 		    b += " " + board[i][j] + " ";
@@ -22,23 +18,56 @@ public class KnightBoard{
 		    b += "" + board[i][j] + " ";
 	    }
 	}
-	return b;
+	return b+"\n";
     }
 
     public void solve(){
+	
 	for (int i = 0; i < board.length; i ++){
 	    for (int j = 0; j < board[i].length; j ++){
-		if(solveH(i,j,1))
+		board[i][j] = 0;
+		if (solveH(i, j, 1))
 		    return;
 	    }
 	}
     }
+
+    private boolean valid(int row, int col, int level){
+	return (row < board.length && row > -1 && col < board[0].length && col > -1 && board[row][col] == 0);
+    }
     
     private boolean solveH(int row, int col, int level){
-	return false;   
+	if (level > board.length * board[0].length)
+	    return true;
+	else if (valid(row, col, level)){
+	    board[row][col] = level;
+	    if (solveH(row + 2, col + 1, level + 1)||
+		solveH(row + 2, col - 1, level + 1)||
+		solveH(row - 2, col + 1, level + 1)||		
+		solveH(row - 2, col - 1, level + 1)||
+		solveH(row + 1, col + 2, level + 1)||
+		solveH(row + 1, col - 2, level + 1)||
+		solveH(row - 1, col + 2, level + 1)||
+		solveH(row - 1, col - 2, level + 1))
+		return true;
+	    else
+		board[row][col] = 0;
+	}
+	return false;
     }
-
+    
     public static void main(String[] args){
-        System.out.println("hello world!");
+	for (int i = 1; i < 8; i ++){
+	    System.out.println("Solving a " + i + " x " + i + " board: \n");
+	    KnightBoard test = new KnightBoard(i,i);
+	    test.solve();
+	    System.out.println(test);
+	}
+	for (int i = 1; i < 8; i ++){
+	    System.out.println("Solving a " + i + " x " + (i+1) + " board: \n");
+	    KnightBoard test = new KnightBoard(i,i+1);
+	    test.solve();
+	    System.out.println(test);
+	}
     }
 }
