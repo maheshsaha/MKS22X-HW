bdc561f5a84cb6ce9ce7a7287f11d8f77ed82d04
Mahesh Saha <sahamahesh20@gmail.com>
2017-02-28 12:19:19 -0500

    minor tweak to toString of NQueens

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -116,7 +116,7 @@ public class QueenBoard{
     public String toString(){
 	String i = "";
 	for(int r = 0; r < board.length; r++){
-	    i += "\n";
+	    if (r != 0)  i += "\n";
 	    for(int c = 0; c < board[0].length; c++){
 		if(board[r][c] == -1){
 		    i += " Q ";}
@@ -128,28 +128,33 @@ public class QueenBoard{
     }
 
     public static void main(String[]args){
-	QueenBoard b;
-	int[]tests =   {2,4, 5,6, 7, 8,  9,10,11};
-	int[]answers = {0,2,10,4,40,92,352,724,2680};
-	int score = 0;
-	for(int i = 0; i < tests.length; i++){
-	    int size = tests[i];
-	    int ans  = answers[i];
-	    b = new QueenBoard(size);
-	    b.countSolutions();
-	    if(b.getSolutionCount()==ans){
-		score++;
-	    }else{
-		System.out.println("Failed board size: "+size );
-	    }
-	}
-	b = new QueenBoard(5);
-	if(b.getSolutionCount() == -1){
-	    score++;
-	}else{
-	    System.out.println("Failed when solution not yet run");
-	}
-	System.out.println("Score: "+score+" / "+(tests.length+1));
+    	QueenBoard b;
+    	int[]tests =   {2,4, 5,6, 7, 8,  9,10,11};
+    	int[]answers = {0,2,10,4,40,92,352,724,2680};
+    	int score = 0;
+    	for(int i = 0; i < tests.length; i++){
+    	    int size = tests[i];
+    	    int ans  = answers[i];
+    	    b = new QueenBoard(size);
+    	    b.countSolutions();
+    	    if(b.getSolutionCount()==ans){
+    		score++;
+    	    }else{
+    		System.out.println("Failed board size: "+size );
+    	    }
+    	}
+    	b = new QueenBoard(5);
+    	if(b.getSolutionCount() == -1){
+    	    score++;
+    	}else{
+    	    System.out.println("Failed when solution not yet run");
+    	}
+    	System.out.println("Score: "+score+" / "+(tests.length+1));
     }
+    // public static void main(String[]args){
+    // 	QueenBoard b = new QueenBoard(8);
+    // 	b.solve();
+    // 	System.out.println(b);
+    // }
 }
 
