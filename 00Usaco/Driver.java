2a3f49e502b8fe95886f9cf9ec1176e299928cb1
Mahesh Saha <sahamahesh20@gmail.com>
2017-03-10 12:26:49 -0500

    Some computational error, gotta fix it

--- 00Usaco/Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver{
-    public static void main(String[]args){
-        USACO g = new USACO();
-	g.bronze("data1.dat");
-	//	System.out.println(g.blah());
-	System.out.println(g.volumeCalc());
-    }
-}
