0d23607a157edd0239da7be3642439190481a80b
Mahesh Saha <sahamahesh20@gmail.com>
2017-03-24 10:29:14 -0400

    Made dir for 08Merge

---
+++ 08Merge/Merge.java
@@ -0,0 +1,29 @@
+public class Merge{
+    public static void merge(int[] a, int[] b, int[] destination){
+	/*
+	  Postcondition:
+	  destination contains all of the elements of a and b, and is sorted. 
+	  Preconditions:
+	  a is sorted, b is sorted
+	  destination.length == a.length + b.length
+	*/
+    }
+
+    public static void mergesort(int[] ary){
+	/*
+	  mergesort the leftHalf
+	  mergesort the rightHalf
+	  mergeTheTwoHalvesIntoTheOriginalArray
+	*/
+    }
+    /*
+      Test on arrays with size greater than hundreds of thousand
+      Try completely random array
+      Random 0 --> maxInt (no dupes)
+      Random 0 --> 10 (dupes)
+      All 0s
+      Sorted array (for loop with increasing index
+      Reverse sorted array
+      (Array size 1 million, with diff kinds of arrays)
+    */
+      
