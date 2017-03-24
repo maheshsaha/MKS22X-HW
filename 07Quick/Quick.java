1f47e6dc2d1d43a033716855ce567be7fa699981
Mahesh Saha <msaha1@stuy.edu>
2017-03-24 02:43:56 -0400

    Finished sort

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -13,7 +13,7 @@ public class Quick {
 	    return start;
 	
 	int randomInd = (int)(Math.random() * (end - start + 1)) + start;
-	System.out.println(randomInd);
+	
 	swap(data,randomInd,start);
 
 	int gt = end;
@@ -59,18 +59,44 @@ public class Quick {
     }
     
     public static void quicksort(int[]data){
+    	quicksortH(data, 0, data.length - 1);
     }
 
+    public static void quicksortH(int[]data, int start, int end){
+    	if (start == end)
+    	    return;
+	
+    	int randomInd = (int)(Math.random() * (end - start + 1)) + start;
+    	
+    	swap(data,randomInd,start);
+	
+    	int gt = end;
+    	int lt = start;
+    	int i = start + 1;
+    	int val = data[lt];
+
+    	while(i <= gt){
+    	    if(data[i] == val){
+    		i++;
+    	    }
+    	    else if(data[i] < val){
+    		swap(data,i,lt);
+    		i++;
+    		lt++;
+    	    }
+    	    else{
+    		swap(data,gt,i);
+    		gt--;
+    	    }
+    	}
+	if (start < end){
+	    quicksortH(data, gt, end);
+	    quicksortH(data, start, lt - 1);
+	}
+    }
+    
+    
+    
     public static void main(String[] args){
-	int[] data = {1,1,1,2,5,0};
-	System.out.println(part(data, 0, 5));
-	System.out.println(Arrays.toString(data));
-	// int[]ary = { 2, 10, 15, 23, 0,  5};
-	// System.out.println(quickselect( ary , 0 ));// would return 0
-	// System.out.println(quickselect( ary , 1 ));//  would return 2
-	// System.out.println(quickselect( ary , 2 ));//  would return 5
-	// System.out.println(quickselect( ary , 3 ));//  would return 10
-	// System.out.println(quickselect( ary , 4 ));//  would return 15
-	// System.out.println(quickselect( ary , 5 ));//  would return 23
     }
 }
