7b13a411a09824a24fbc46db4e3351ba9961eae2
Mahesh Saha <msaha1@stuy.edu>
2017-03-28 23:36:12 -0400

    Don't know why Phase II add isn't working

--- 09Linked/MyLinkedList.java
+++ 09Linked/MyLinkedList.java
@@ -16,20 +16,65 @@ public class MyLinkedList {
     }
     
     private LNode start;
+    private LNode end;
     private int size;
 
     public MyLinkedList(){
 	start = null;
+	end = null;
 	size = 0;
     }
     
-    public boolean add (int value){ //adds to start
-       	LNode i = new LNode(value, start);
-        start = i;
+    public boolean add (int value){ //adds to end now!
+	if (start == null)
+	    start = new LNode(value);
+	else{
+	    LNode current = start;
+	    for (int i = 0; i < size(); i ++){
+		if (current.next == null){
+		    current.next = new LNode(value);
+		    end = new LNode(value);
+		}
+		current = current.next;
+	    }
+	}
 	size ++;
 	return true;
     }
 
+    
+    public void add(int index, int value){
+	LNode current = start;
+	if(index < 0 || index > size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	else if (index == size()){
+	    for (int i = 0; i < size(); i ++){
+		if (current.next == null){
+		    current.next = new LNode(value);
+		    end = new LNode(value);
+		}
+		current = current.next;
+	    }
+	}
+	else if (index == 0){
+	    start = new LNode(value, start);
+	}
+
+	else{
+	    int temp = set(index, value);
+	    for(int j = 0; j < size(); j ++){
+		if (j >= index + 1){
+		    temp = set(j, temp);
+		}
+		current = current.next;
+	    }
+	    current = new LNode(temp,null);
+	    end = new LNode(temp,null);
+	}
+	size ++;
+    }
+    
        
     public int size(){
 	return size;
@@ -102,7 +147,7 @@ public class MyLinkedList {
 	x.add(3);
 	x.add(4);
 	x.add(5);
-
+	x.add(4,6);
 	System.out.println(x);
 	System.out.println(x.set(0,3));
 	System.out.println(x);
