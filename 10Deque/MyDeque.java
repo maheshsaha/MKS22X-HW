f5a7aa00b8ea742bc8c8c8bcbe0b08cc7a9e9b1e
Mahesh Saha <msaha1@stuy.edu>
2017-04-19 12:33:42 -0400

    Blank methods

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,13 @@
+public class MyDeque{
+    private int front, back;
+
+    public void addFirst(String i){
+    }
+    
+    public void addLast(String i){
+    }
+    
+    public String removeFirst(){
+    }
+
+}
