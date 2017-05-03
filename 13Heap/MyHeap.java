58918a2a5afc5cd3ca698c65dfca5aa32debc7e3
Mahesh Saha <msaha1@stuy.edu>
2017-05-03 12:33:02 -0400

    Made the method shell

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MyHeap{
+
+    boolean minMax; //false means max heap, true means min heap
+    
+    public MyHeap(){
+	minMax = false;
+    }
+
+    public MyHeap(boolean value){
+        minMax = value;
+    }
+
+    public void add(String s){
+    }
+    
+    public String remove(){
+    }
+
+    public void peek(){
+    }
+
+    public void pushForth(){
+    }
+
+    public void pushBack(){
+    }
+    
+    public static void main(String[]args){
+    }
+
+}
+
+
+    
