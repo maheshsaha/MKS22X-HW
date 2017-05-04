08ed6fa46671e2cc7672c22c7518e78162bfb71a
Mahesh Saha <msaha1@stuy.edu>
2017-05-04 12:28:22 -0400

    Made some progress, but very confused

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -2,29 +2,63 @@ import java.util.*;
 
 public class MyHeap{
 
-    boolean minMax; //false means max heap, true means min heap
+    boolean minMax; //true means max heap
+    ArrayList<String> heap;
+    int min, max;
+    int current;
     
     public MyHeap(){
-	minMax = false;
+	minMax = true;
+	heap = new ArrayList<String>()
     }
 
     public MyHeap(boolean value){
         minMax = value;
+	heap = new ArrayList<String>()
     }
 
     public void add(String s){
+	int child1 = current * 2;
+	int child2 = child1 + 1;
+	if (minmax)
+	    if(max == null){
+		max = heap.get(1);
+		current = 1;
+	    else{
+		if (heap.get(child1) == null)
+		    heap.add(child1, s);
+		else if (heap.get(child2) == null){
+		    heap.add(child2, s);
+		    current += 1;
+		}
+		if (s.compareTo(max)>0)
+		    pushForth();
+		else
+		    pushBack();
+	else
+	    if(min == null)
+		min = heap.get(1);
+	    else
+		if (s.compareTo(min)<0)
+		    min = s;
+		else
+		    pushBack();
     }
     
     public String remove(){
     }
 
-    public void peek(){
+    public String peek(){
     }
 
-    public void pushForth(){
+    public void pushForth(int index){
+	int child1 = index * 2;
+	int child2 = child1 + 1;
+	if(minMaX)
+	    if (heap.get(index).compareTo(heap.get(child1))
     }
 
-    public void pushBack(){
+    public void pushBack(int index){
     }
     
     public static void main(String[]args){
